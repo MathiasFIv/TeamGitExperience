a9dcf6ae79597c681889af9fdf349636e4f0b3e4
Casper <casfro01@easv365.dk>
2024-11-14 10:22:53 +0100

    Suck my balls

--- src/main/java/tv/safte/teamgitexperiene1/HelloApplication.java
+++ src/main/java/tv/safte/teamgitexperiene1/HelloApplication.java
@@ -8,6 +8,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    /**
+     * I love refrigerators
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException because you probably succ
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
